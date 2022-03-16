cd9af68ccc977cd73d4e25ed5901e08ac5bd8bae
Emin-Modoglu <eminmodogluxox@gmail.com>
2022-03-16 21:59:11 +0200

    sistem_bilgisi

---
+++ learn_computer_features.py
@@ -0,0 +1,24 @@
+import platform
+
+# İşletim sistemini öğrenmek için -->  
+print(platform.system())
+
+
+# İşlemci bilgisi öğrenmek için -->
+print(platform.processor())
+
+
+# Python Versiyonu öğrenmek için -->
+print(platform.python_version())
+
+
+# Bilgisayarın kullanıcı adını öğrenmek için -->
+print(platform.node())
+
+
+#  Python compiler bilgisini öğrenmek için -->
+print(platform.python_compiler())
+
+
+# Python implementasyonu öğrenmek için -->
+print(platform.python_implementation())
